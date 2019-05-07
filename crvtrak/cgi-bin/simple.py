69d32129ab288e3f9a7d8f06d75a25ef33102c0f
John Peterson <jp@adobe.com>
2019-05-07 13:58:14 -0700

    Bare test

---
+++ crvtrak/cgi-bin/simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+print "Content-type: text/html"
+print ""
+print """
+<html>
+<head><title>Simple CGI output</title></head>
+<body>
+<h1>It's alive - python2</h1>
+</body>
+</html>
+"""
